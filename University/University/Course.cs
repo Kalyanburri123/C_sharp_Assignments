77b0f7fcd7c44dc02dadaf7f10f1376cb35bb8af
DELL <kalyanburri874@gmail.com>
2026-03-17 06:35:32 +0530

    University course enrollment program uploaded

---
+++ University/University/Course.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University
+{
+    abstract class Course
+    {
+        protected string CourseName;
+        protected string Instructor;
+
+        public Course(string courseName, string instructor)
+        {
+            CourseName = courseName;
+            Instructor = instructor;
+        }
+
+        public abstract void EnrollStudent();
+    }
+    class OnlineCourse : Course
+    {
+        string VideoPlatformLink;
+
+        public OnlineCourse(string name, string instructor, string link)
+            : base(name, instructor)
+        {
+            VideoPlatformLink = link;
+        }
+
+        public override void EnrollStudent()
+        {
+            if (string.IsNullOrEmpty(VideoPlatformLink))
+            {
+                Console.WriteLine("Online course must have a video platform link.");
+            }
+            else
+            {
+                Console.WriteLine($"Student enrolled in Online Course: {CourseName}");
+                Console.WriteLine("Video Link: " + VideoPlatformLink);
+            }
+        }
+    }
+    class InPersonCourse : Course
+    {
+        string RoomNumber;
+        int Capacity;
+
+        public InPersonCourse(string name, string instructor, string room, int capacity)
+            : base(name, instructor)
+        {
+            RoomNumber = room;
+            Capacity = capacity;
+        }
+
+        public override void EnrollStudent()
+        {
+            if (Capacity <= 0)
+            {
+                Console.WriteLine("Invalid room capacity.");
+            }
+            else
+            {
+                Console.WriteLine($"Student enrolled in In-Person Course: {CourseName}");
+                Console.WriteLine($"Room: {RoomNumber} | Capacity: {Capacity}");
+            }
+        }
+    }
+    class LabCourse : Course
+    {
+        bool SafetyTrainingCompleted;
+
+        public LabCourse(string name, string instructor, bool safetyTraining)
+            : base(name, instructor)
+        {
+            SafetyTrainingCompleted = safetyTraining;
+        }
+
+        public override void EnrollStudent()
+        {
+            if (!SafetyTrainingCompleted)
+            {
+                Console.WriteLine("Safety training must be completed before enrolling.");
+            }
+            else
+            {
+                Console.WriteLine($"Student enrolled in Lab Course: {CourseName}");
+            }
+        }
+    }
+}
---
+++ University/University/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace University
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select Course Type:");
+            Console.WriteLine("1. Online Course");
+            Console.WriteLine("2. In-Person Course");
+            Console.WriteLine("3. Lab Course");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Course Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Instructor Name: ");
+            string instructor = Console.ReadLine();
+
+            Course course = null;
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter Video Platform Link: ");
+                    string link = Console.ReadLine();
+                    course = new OnlineCourse(name, instructor, link);
+                    break;
+
+                case 2:
+                    Console.Write("Enter Room Number: ");
+                    string room = Console.ReadLine();
+
+                    Console.Write("Enter Capacity: ");
+                    int cap = Convert.ToInt32(Console.ReadLine());
+
+                    course = new InPersonCourse(name, instructor, room, cap);
+                    break;
+
+                case 3:
+                    Console.Write("Safety Training Completed (true/false): ");
+                    bool training = Convert.ToBoolean(Console.ReadLine());
+
+                    course = new LabCourse(name, instructor, training);
+                    break;
+            }
+
+            course.EnrollStudent();
+        }
+    }
+}
