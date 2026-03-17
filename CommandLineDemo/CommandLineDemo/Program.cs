ba873d29b0ca76b6f4e8d09aca90b239a8e2c811
DELL <kalyanburri874@gmail.com>
2026-03-17 09:45:08 +0530

    Added Basic commandLine Program

---
+++ CommandLineDemo/CommandLineDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CommandLineDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            int average = 0;
+            int max = 0;
+            int min = int.Parse(args[0]);
+            int count = args.Length;
+            Console.WriteLine($"The count of total numbers are :{count}");
+            for(int i=0;i<args.Length; i++)
+            {
+
+                sum = sum + int.Parse(args[i]);
+            }
+            for(int i=0;i<args.Length;i++)
+            {
+                max = Math.Max(max, int.Parse(args[i]));
+                min=Math.Min(min,int.Parse(args[i]));
+            }
+            Console.WriteLine($"The sum of Numbers is {sum}");
+            Console.WriteLine($"The average of numbers is {(double)sum / count}");
+            Console.WriteLine($"The max of Numbers is {max}");
+            Console.WriteLine($"The min of Numbers is {min}");
+        }
+    }
+}
