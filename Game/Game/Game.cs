3391ce0c17a06e9e6edc1a1d7e19166a1417a547
DELL <kalyanburri874@gmail.com>
2026-03-24 10:38:18 +0530

    Added PigGame Program

---
+++ Game/Game/Game.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace PigDiceGame
+{
+    class Game
+    {
+        private int totalScore = 0;
+        private int turn = 1;
+        private Random rand = new Random();
+
+        public void StartGame()
+        {
+            while (totalScore < 20)
+            {
+                Console.WriteLine($"\nTURN {turn}");
+                PlayTurn();
+                turn++;
+            }
+
+            Console.WriteLine($"\nYou finished in {turn - 1} turns!");
+            Console.WriteLine("Game over!");
+        }
+
+        private void PlayTurn()
+        {
+            int turnScore = 0;
+            bool turnOver = false;
+
+            while (!turnOver)
+            {
+                char choice = GetUserChoice();
+
+                if (choice == 'r')
+                {
+                    int die = rand.Next(1, 7);
+                    Console.WriteLine("Die: " + die);
+
+                    if (die == 1)
+                    {
+                        Console.WriteLine("Turn over. No score.");
+                        turnScore = 0;
+                        turnOver = true;
+                    }
+                    else
+                    {
+                        turnScore += die;
+                    }
+                }
+                else if (choice == 'h')
+                {
+                    Console.WriteLine("Score for turn: " + turnScore);
+                    totalScore += turnScore;
+                    Console.WriteLine("Total score: " + totalScore);
+                    turnOver = true;
+                }
+            }
+        }
+
+        private char GetUserChoice()
+        {
+            while (true)
+            {
+                Console.Write("Roll or hold? (r/h): ");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "r" || input == "h")
+                {
+                    return input[0];
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Enter 'r' or 'h'");
+                }
+            }
+        }
+    }
+}
---
+++ Game/Game/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PigDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();  
+            game.StartGame();       
+        }
+    }
+}
