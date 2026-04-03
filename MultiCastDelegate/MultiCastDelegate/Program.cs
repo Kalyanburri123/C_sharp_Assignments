1e9b3214d96233a9c176ecf3a1a1d7229827965e
DELL <kalyanburri874@gmail.com>
2026-04-03 18:34:06 +0530

    Added MultiCast Delegate Program

---
+++ MultiCastDelegate/MultiCastDelegate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace MultiCastDelegate
+{
+    class Program
+    {
+
+        //Declared a delegate.
+        public delegate void Notify(string message);
+        public static void Main(string[] args)
+        {
+            // Multicast delegate
+            Notify notify = new Notify(SendEmail);
+
+            // Adding methods
+            //notify += SendEmail;
+            notify += SendSMS;
+            notify += SendWhatsApp;
+
+            // Invoking the delegate
+            notify("Order Placed");
+
+            // Remove SMS
+            notify -= SendSMS;
+
+            Console.WriteLine("---- After Removing SMS ----");
+            notify("Order Shipped");
+            notify -= SendWhatsApp;
+            Console.WriteLine("---- After Removing Whatsapp ----");
+            notify("Order Received");
+        }
+        public static void SendEmail(string message)
+        {
+            Console.WriteLine("Email Sent: " + message);
+        }
+
+        public static void SendSMS(string message)
+        {
+            Console.WriteLine("SMS Sent: " + message);
+        }
+
+        public static void SendWhatsApp(string message)
+        {
+            Console.WriteLine("WhatsApp Sent: " + message);
+        }
+    }
+}
